bb5c62378aabf340271e3c05eaeec01a0825a242
Mateusz Wójcikowski <yankes90@gmail.com>
2022-12-21 14:37:02 +0100

    turnover

--- src/index.js
+++ src/index.js
@@ -1,52 +1,46 @@
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-import Notiflix from 'notiflix';
+import './css/styles.css';
 import axios from 'axios';
-import { key } from './partials/pass';
+import { Chart } from 'chart.js/auto';
 
 const gallery = document.querySelector('.gallery');
-const form = document.querySelector('.search-form');
-const input = document.querySelector('input');
-const loader = document.querySelector('.load-more');
-const switcher = document.querySelector('.switch');
-const imagesPerPage = 40;
+const date = document.querySelector('.date');
+const loader = document.querySelector('.load');
 
-let infinityCheck = switcher.innerHTML === 'Image Loader: Infinite Scroll';
-let i = 1;
-let end = null;
-let inputQuery = null;
+const ctx = document.getElementById('myChart');
+const ctx1 = document.getElementById('myChart1');
+const ctx2 = document.getElementById('myChart2');
+const ctx3 = document.getElementById('myChart3');
 
-form.addEventListener('submit', handleSubmit);
-loader.addEventListener('click', loadMoreImages);
-switcher.addEventListener('click', switchingMode);
+// loader.addEventListener('click', loadData);
 
-const fetchPictures = async (name, page = 1) => {
+const fetchData = async () => {
+  const params = 'A/last/66/';
   const table = await axios.get(
-    `https://pixabay.com/api/?key=${key}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${imagesPerPage}`
+    `
+https://api.nbp.pl/api/exchangerates/tables/${params}/`
   );
+  console.log(table)
   return table;
 };
 
-var lightbox = new SimpleLightbox('.gallery a', {
-  captionDelay: 250,
-  captionsData: 'alt',
-});
+//https://pixabay.com/api/?key=6755131-7999fe22e3bb9fa8947c67297&q=yellow+flowers&image_type=photo`
+// https://api.nbp.pl/api/exchangerates/tables/${params}/
 
-function loadMoreImages() {
-  i++;
-  fetchPictures(inputQuery, i)
+// const exe = function () {
+//   loadData();
+// };
+// exe();
+
+function loadData() {
+  fetchData()
     .then(function (response) {
       // handle success
-      renderImages(response);
-      if (
-        imagesPerPage * i >= response.data.totalHits &&
-        response.data.totalHits !== 0
-      ) {
-        end = true;
-        Notiflix.Notify.warning(
-          "We're sorry, but you've reached the end of search results."
-        );
-      }
+      renderVaules(response);
+      getChartData(response, 1, 'red', ctx, 'Dolar amerykański');
+      getChartData(response, 7, 'blue', ctx1, 'Euro');
+      getChartData(response, 9, 'green', ctx2, 'Frank szwajcarski');
+      getChartData(response, 10, 'purple', ctx3, 'Funt szterling');
+      console.log(response);
     })
     .catch(function (error) {
       // handle error
@@ -54,104 +48,64 @@ function loadMoreImages() {
     });
 }
 
-function handleSubmit(event) {
-  event.preventDefault();
-  inputQuery = input.value;
-  i = 1;
-  if (!infinityCheck) { end = false };
-  gallery.innerHTML = '';
-  fetchPictures(inputQuery)
-    .then(function (response) {
-      // handle success
-      if (response.data.hits.length === 0) {
-        Notiflix.Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-        return;
-      }
-      renderImages(response);
-      Notiflix.Notify.success(
-        `Hooray! We found ${response.data.totalHits} images.`
-      );
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    });
-}
+function renderVaules(response) {
+  const table = response.data.length;
+  const day = `<h1>Kurs na dzień ${
+    response.data[table - 1].effectiveDate
+  }</h1>`;
 
-function renderImages(response) {
-  const markupList = response.data.hits
-    .map(
-      image => `<div class="photo-card">
-   <a href="${image.largeImageURL}"><img class="album" src="${image.webformatURL}" alt="${image.tags}" loading="lazy" /></a>
-  <div class="info">
-    <p class="info-item">
-      <b>Likes</b> ${image.likes}
-    </p>
-    <p class="info-item">
-      <b>Views</b> ${image.views}
-    </p>
-    <p class="info-item">
-      <b>Comments</b> ${image.comments}
-    </p>
-    <p class="info-item">
-      <b>Downloads</b> ${image.downloads}
-    </p>
-  </div>
-</div>`
+  const markupList = response.data[table - 1].rates
+    .filter(
+      image =>
+        image.code === 'USD' ||
+        image.code === 'EUR' ||
+        image.code === 'CHF' ||
+        image.code === 'GBP'
     )
-    .join('');
+    .map(
+      image => `<div class="column"><p class = "font"><b>${capitalizeFirstLetter(
+        image.currency
+      )}</b></p>
+      <p class = "font">1 ${image.code} = ${image.mid} PLN</p></div>
+   `
+    )
+    .join(``);
+  date.insertAdjacentHTML('beforeend', day);
   gallery.insertAdjacentHTML('beforeend', markupList);
-  lightbox.refresh();
-  window.removeEventListener('scroll', handleInfiniteScroll);
-  window.addEventListener('scroll', handleInfiniteScroll);
 }
 
-loader.classList.toggle('hidden');
-function switchingMode() {
-  if (switcher.innerHTML === 'Image Loader: Infinite Scroll') {
-    switcher.innerHTML = 'Image Loader: Button';
-    loader.classList.toggle('hidden');
-    end = true;
-  } else {
-    switcher.innerHTML = 'Image Loader: Infinite Scroll';
-    end = false;
-    loader.classList.toggle('hidden');
-    window.removeEventListener('scroll', handleInfiniteScroll);
-    window.addEventListener('scroll', handleInfiniteScroll);
+function getChartData(response, number, color, id, currency) {
+  const history = [];
+  for (let i = 0; i <= response.data.length - 1; i++) {
+    history.push({
+      day: response.data[i].effectiveDate,
+      value: response.data[i].rates[number].mid,
+    });
   }
+  (async function () {
+    const data = history;
+    new Chart(id, {
+      type: 'line',
+      data: {
+        labels: data.map(row => row.day),
+
+        datasets: [
+          {
+            label: `
+        ${capitalizeFirstLetter(currency)}`,
+            data: data.map(row => row.value),
+            borderColor: color,
+            backgroundColor: color,
+          },
+        ],
+      },
+    });
+  })();
 }
 
-//Infinite Scroll and Throttle
-var throttleTimer;
-const throttle = (callback, time) => {
-  if (throttleTimer) return;
+function capitalizeFirstLetter(string) {
+  return string[0].toUpperCase() + string.slice(1);
+}
 
-  throttleTimer = true;
 
-  setTimeout(() => {
-    callback();
-    throttleTimer = false;
-  }, time);
-};
-
-const handleInfiniteScroll = () => {
-  throttle(() => {
-    const endOfPage =
-      window.innerHeight + window.pageYOffset >= document.body.offsetHeight;
-
-    if (endOfPage) {
-      loadMoreImages();
-      console.log('scroll');
-    }
-
-    if (end) {
-      removeInfiniteScroll();
-    }
-  }, 1000);
-};
-
-const removeInfiniteScroll = () => {
-  window.removeEventListener('scroll', handleInfiniteScroll);
-};
+window.onload = loadData;
